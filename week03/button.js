638007142f98a1464147775975eb31191b178698
Alexander Berryhill <amsb99@gmail.com>
2022-01-21 10:49:24 -0700

    Finished assign03

--- week03/button.js
+++ week03/button.js
@@ -1,7 +1,35 @@
-function w3_open() {
+
+
+//sidebar functions
+function openSidebar() {
     document.getElementById("mySidebar").style.display = "block";
 }
   
-function w3_close() {
+function closeSidebar() {
     document.getElementById("mySidebar").style.display = "none";
+}
+
+//open the Form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+var modalForm = document.getElementById('myForm');
+
+// When the user clicks anywhere outside of the form, close it
+window.onclick = function(event) {
+  if (event.target == document.getElementById('myForm')) {
+    document.getElementById('myForm').style.display = "none";
+  }
+}
+
+//Go to top button
+// When the user clicks on the button, scroll to the top of the page
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
