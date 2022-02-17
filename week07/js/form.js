fa4b89b0cbabd3ac14b9fa750f33c44d603e9ad3
Alexander Berryhill <amsb99@gmail.com>
2022-02-17 10:39:08 -0700

    added required

--- week07/js/form.js
+++ week07/js/form.js
@@ -1,9 +1,20 @@
-const createAPR = () => {
+//Calculate Monthly Interest
+const apr = document.querySelector('#apr');
+const term = document.querySelector('#term');
+const amount = document.querySelector('#amount');
+const payment = document.querySelector('#payment');
+document.getElementById("calculate").addEventListener('click', calcMonthlyPayment);
+
+function calcMonthlyPayment(){
+    alert('hit');
+    payment.value = 6;
+}
+
+function createAPR() {
     let apr = document.querySelector('.form');
 
     apr.innerHTML = `
         <div class="form">
         </div>
-        `
-    ;
+        `;
 }
