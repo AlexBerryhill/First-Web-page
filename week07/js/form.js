4957091a75eb98f81aa39cfb6c772c69bb60ffe1
Alexander Berryhill <amsb99@gmail.com>
2022-02-17 18:34:00 -0700

    math

--- week07/js/form.js
+++ week07/js/form.js
@@ -1,13 +1,14 @@
 //Calculate Monthly Interest
-var apr = document.getElementById('apr').value;
+
 const term = document.getElementById('term');
 const amount = document.getElementById('amount');
 
-document.getElementById("calculate").addEventListener('click', calcMonthlyPayment);
-
 function calcMonthlyPayment(){
-    document.getElementById('payment').value=apr
-    alert(apr.value)
+    var i = document.getElementById('apr').value/100/12;
+    var n = document.getElementById('term').value*12;
+    var a = document.getElementById('amount').value;
+    var x = (1+i)**n
+    document.getElementById('payment').value="$"+parseFloat((a*x*i)/(x-1)).toFixed(2);
 }
 
 function createAPR() {
