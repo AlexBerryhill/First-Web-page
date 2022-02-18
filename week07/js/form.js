9cb6522b89427f059928eab25f6f50dbbf8f6f6b
Alexander Berryhill <amsb99@gmail.com>
2022-02-18 11:27:35 -0700

    Added val messages

--- week07/js/form.js
+++ week07/js/form.js
@@ -3,6 +3,39 @@
 const term = document.getElementById('term');
 const amount = document.getElementById('amount');
 
+function valApr(){
+    var i = document.getElementById('apr').value
+    if (isNaN(i) || i < 0.01 || i > 100.00) {
+        document.getElementById("aprError").innerHTML = "Input not valid";
+        return false;
+      } else {
+        document.getElementById("aprError").innerHTML = "";
+        return true;
+      }
+}
+
+function valTerm(){
+    var n = document.getElementById('term').value
+    if (isNaN(n) || n < 1 || n > 100) {
+        document.getElementById("termError").innerHTML = "Input not valid";
+        return false;
+      } else {
+        document.getElementById("termError").innerHTML = "";
+        return true;
+      }
+}
+
+function valAmount(){
+    var a = document.getElementById('amount').value
+    if (isNaN(a) || a < 0) {
+        document.getElementById("amountError").innerHTML = "Input not valid";
+        return false;
+      } else {
+        document.getElementById("amountError").innerHTML = "";
+        return true;
+      }
+}
+
 function calcMonthlyPayment(){
     var i = document.getElementById('apr').value/100/12;
     var n = document.getElementById('term').value*12;
