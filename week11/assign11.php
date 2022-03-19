9fe24b98b14d70be1659cd46a7b4c296d771114c
Alexander Berryhill <amsb99@gmail.com>
2022-03-18 19:55:28 -0600

    Changed headings

--- week11/assign11.php
+++ week11/assign11.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title style="font-family: Arial, Helvetica, sans-serif;">PHP Mortgage</title>
+        <title style="font-family: Arial, Helvetica, sans-serif;">Confirm Order</title>
         <meta name="description" content="">
         <link rel="icon" href="./img/dark-logo.png" type="image/png">
         <link rel="stylesheet" href="stylesheet.css">
@@ -11,8 +11,9 @@
     <body>
         <nav class="navbar"></nav>
         <script src="js/nav.js"></script>
+        <h1>Confirm Order</h1>
         <div class="results">
-            <h1>Confirm</h1>
+            <h1>Results</h1>
             
             <?php
                 echo "<br>Name: ".$_POST['first_name']." ".$_POST["last_name"];
