7bc37db5744da154851d20395c71e3775dc51187
Alexander Berryhill <amsb99@gmail.com>
2022-03-30 17:33:18 -0600

    Fixed style and php

--- week13/assign13.php
+++ week13/assign13.php
@@ -7,7 +7,6 @@
     <meta name="description" content="Piano Signup">
     <link rel="icon" href="./img/dark-logo.png" type="image/png">
     <link rel="stylesheet" href="stylesheet.css">
-    <script src="php.js"></script>
 </head>
 <body>
     <?php
@@ -16,15 +15,50 @@
             $json_arr=json_decode($data, true);
             return $json_arr;
         }
+        
+        function getName($performance){
+            if($performance == "Duet"){
+                return $_GET['first_name']." ".$_GET['last_name']." and ".$_GET['first_name_2']." ".$_GET['last_name_2'];
+            }
+            else{
+                return $_GET['first_name']." ".$_GET['last_name'];
+            }
+        }
+
+        function getPerformance($performance){
+            $text = $performance." for\n".$_GET['skill']." ".$_GET['instrument'];
+            if($performance == "Duet"){
+                $text .= " and ".$_GET['skill_2']." ".$_GET['instrument_2'];
+            }
+            return $text;
+        }
+
+        function toTable($data){
+            $table = "<table><tr><th>Name</th><th>Location</th><th>Time</th><th>Performance</th></td>";
+            for($i=0;$i<count($data);$i++){
+                $table .="<tr><td>".$data[$i]["name"]."</td>".
+                        "<td>".$data[$i]["location"]."</td>".
+                        "<td>".$data[$i]["time"]."</td>".
+                        "<td>".$data[$i]["performance"]."</td></tr>";
+            }
+            $table .= "</table>";
+            return $table;
+        }
 
         function writeFile($data, $filename){
             file_put_contents($filename, json_encode($data));
         }
+
         $filename = "./data/sheet.json";
         $data = getFile($filename);
-        $data[] = array('name'=>$_GET('first_name')." ".$_GET('last_name'),
-                        'location'=>$_GET('location')."\nRoom ".$_GET('room'),
-                        'time'=>$_GET('time_slot'),
-                        'performance'=>$_GET('performance')." for\n".$_GET('skill')." ".$_GET('instrument'))
+        $name = getName($_GET['performance']);
+        $performance = getPerformance($_GET['performance']);
+        $data[] = ['name'=>$name,
+                    'location'=>$_GET['location']."\nRoom ".$_GET['room'],
+                    'time'=>$_GET['time_slot'],
+                    'performance'=>$performance];
+        writeFile($data, $filename);
+        $table = toTable($data);
+        echo $table;
     ?>
 </body>
--- week13/js/form.js
+++ week13/js/form.js
@@ -1,5 +1,21 @@
+function activateDuet(){
+    document.getElementById("student_2").style.display = "block";
+    document.getElementById("first_name_2").setAttribute('required', '');
+    document.getElementById("last_name_2").setAttribute('required', '');
+    document.getElementById("skill_2").setAttribute('required', '');
+    document.getElementById("instrument_2").setAttribute('required', '');
+}
+
+function deactivateDuet(){
+    document.getElementById("student_2").style.display = "none";
+    document.getElementById("first_name_2").removeAttribute('required', '');
+    document.getElementById("last_name_2").removeAttribute('required', '');
+    document.getElementById("skill_2").removeAttribute('required', '');
+    document.getElementById("instrument_2").removeAttribute('required', '');
+}
+
 function performanceChange(){
     let value = document.getElementById("performance").value;
-    if (value == "Duet") document.getElementById("student_2").style.display = "block";
-    else document.getElementById("student_2").style.display = "none";
+    if (value == "Duet") activateDuet();
+    else deactivateDuet();
 }
